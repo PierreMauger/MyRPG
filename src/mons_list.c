66b6e22794056b92138c0ac701cda20b81b595a1
Pierre <pierre.mauger@epitech.eu>
2021-01-24 01:03:01 +0100

    add origin position on monsters

--- src/mons_list.c
+++ src/mons_list.c
@@ -30,6 +30,8 @@ void put_in_p_mons_list(p_mons_t **p_mons, sfVector2f pos, char name)
     element->sprite = sfSprite_create();
     element->texture_color = sfTexture_createFromFile(data_mons[i].sprite_color, NULL);
     element->sprite_color = sfSprite_create();
+    sfSprite_setOrigin(element->sprite, (sfVector2f){20, 40});
+    sfSprite_setOrigin(element->sprite_color, (sfVector2f){20, 40});
     sfSprite_setPosition(element->sprite, pos);
     sfSprite_setPosition(element->sprite_color, pos);
     sfSprite_setColor(element->sprite_color, sfColor_fromRGB(data_mons[i].red, data_mons[i].green, data_mons[i].blue));
@@ -47,6 +49,8 @@ void put_in_e_mons_list(e_mons_t **e_mons, sfVector2f pos, char name)
     element->sprite = sfSprite_create();
     element->texture_color = sfTexture_createFromFile(data_mons[i].sprite_color, NULL);
     element->sprite_color = sfSprite_create();
+    sfSprite_setOrigin(element->sprite, (sfVector2f){20, 40});
+    sfSprite_setOrigin(element->sprite_color, (sfVector2f){20, 40});
     sfSprite_setPosition(element->sprite, pos);
     sfSprite_setPosition(element->sprite_color, pos);
     sfSprite_setColor(element->sprite_color, sfColor_fromRGB(data_mons[i].red, data_mons[i].green, data_mons[i].blue));
