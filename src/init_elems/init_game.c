212e3ff08ef395ade32787e60bac4195dad74990
Pierre <pierre.mauger@epitech.eu>
2021-03-25 11:42:08 +0100

    fix font problem

--- src/init_elems/init_game.c
+++ src/init_elems/init_game.c
@@ -12,8 +12,8 @@ void init_game(game_t *game)
     init_window(game);
     init_set(game);
     init_time(game);
-    init_mons(game);
-    init_turn_ind(game);
     game->mouse_pos = (sfVector2i){0, 0};
     game->font = sfFont_createFromFile(FONT);
+    init_mons(game);
+    init_turn_ind(game);
 }
