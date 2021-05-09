51b6507fe23e66ef367b4da7a092c782ff14339d
Pierre <pierre.mauger@epitech.eu>
2021-05-09 19:41:36 +0200

    FEAT: exit button

--- main.c
+++ main.c
@@ -13,10 +13,10 @@ int main(void)
     game_t *game = malloc(sizeof(game_t));
     sfIntRect rect = {0, 0, 156, 56};
     button_t **list = list_button(2);
-    list[0] = create_button((sfVector2f){320, 50}, "play_button.jpg", rect);
-    list[1] = create_button((sfVector2f){320, 150}, "play_button.jpg", rect);
     int menu_return = 0;
 
+    list[0] = create_button((sfVector2f){320, 50}, "play_button.jpg", rect);
+    list[1] = create_button((sfVector2f){320, 150}, "exit_button.png", rect);
     init_game(game);
     menu_return = menu_loop(game, list);
     if (menu_return != 2)
